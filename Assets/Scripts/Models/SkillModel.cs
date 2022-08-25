0669569c17402144f828bf83b5431f36bb380e5e
Vitaly S <vartovsk72@gmail.com>
2022-08-25 19:44:15 +0300

    add Models

---
+++ Assets/Scripts/Models/SkillModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Models
+{
+    [Serializable]
+    public class SkillModel
+    {
+        public List<int> NeighbourIndex => _neighbourIndex;
+
+        public bool IsBaseSkill => _isBaseSkill;
+        public int Cost => _cost;
+        public Vector2 Position => _position;
+        
+        [SerializeField] private Vector2 _position;
+        [SerializeField] private bool _isBaseSkill;
+        [SerializeField] private int _cost;
+        [SerializeField] private List<int> _neighbourIndex;
+    }
+}
---
+++ Assets/Scripts/Models/SkillTreeModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Models
+{
+    [CreateAssetMenu(fileName = "SkillTreeModel", menuName = "ScriptableObjects/SkillTreeModel")]
+    public class SkillTreeModel : ScriptableObject
+    {
+        public IReadOnlyList<SkillModel> SkillModels => _skillModels;
+        
+        [SerializeField] private List<SkillModel> _skillModels; 
+    }
+}
